251679eafb29cae43ed460de564cda77499e3e8b
MONGSEALE <ghkdehdgur01@gmail.com>
2023-03-29 12:08:22 +0900

    dis

---
+++ GatherUp/dsddassdView.swift
@@ -0,0 +1,20 @@
+//
+//  dsddassdView.swift
+//  GatherUp
+//
+//  Created by DaelimCI10001 on 2023/03/29.
+//
+
+import SwiftUI
+
+struct dsddassdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct dsddassdView_Previews: PreviewProvider {
+    static var previews: some View {
+        dsddassdView()
+    }
+}
