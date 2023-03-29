3f7ae667279ee1974db2aee116e164e7c0b22f69
MONGSEALE <ghkdehdgur01@gmail.com>
2023-03-29 11:14:01 +0900

    Initial Commit

---
+++ GatherUp/MeetingsView.swift
@@ -0,0 +1,46 @@
+//
+//  MeetingsView.swift
+//  GatherUp
+//
+//  Created by DaelimCI10001 on 2023/03/17.
+//
+
+import SwiftUI
+
+struct MeetingsView: View {
+    var body: some View {
+        
+        
+        
+        VStack(spacing:30){
+            
+            
+            Text("This is meetings!")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+            Text("meeting1...........................................")
+                .underline()
+        }
+    }
+}
+
+struct MeetingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingsView()
+    }
+}
---
+++ GatherUp/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  GatherUp
+//
+//  Created by DaelimCI10001 on 2023/03/17.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("This is profile!")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
