c771f002b7a91902d0b8585c52ddb63bdf477fbb
SonJanghyuk <129241778+SonJanghuyk@users.noreply.github.com>
2023-03-29 12:06:07 +0900

    fff

---
+++ GatherUp/ggg.swift
@@ -0,0 +1,20 @@
+//
+//  ggg.swift
+//  GatherUp
+//
+//  Created by DaelimCI00007 on 2023/03/29.
+//
+
+import SwiftUI
+
+struct ggg: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ggg_Previews: PreviewProvider {
+    static var previews: some View {
+        ggg()
+    }
+}
