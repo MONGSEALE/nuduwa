b4805d8e18ceca885efdb4c883d205dc022d15b0
SonJanghyuk <129241778+SonJanghuyk@users.noreply.github.com>
2023-05-03 12:20:31 +0900

    프로필 수정

--- GatherUp/View/Profile/ReusableProfileContent.swift
+++ GatherUp/View/Profile/ReusableProfileContent.swift
@@ -11,43 +11,60 @@ import SDWebImageSwiftUI
 struct ReusableProfileContent: View {
     var user: User
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            LazyVStack{
-                HStack(spacing: 12){
-                    WebImage(url: user.userImage).placeholder{
-                        // MARK: Placeholder Image
-                        Image("NullProfile")
-                            .resizable()
-                    }
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle())
-                    
-                    VStack(alignment: .leading, spacing: 6){
-                        Text(user.userName)
-                            .font(.title3)
-                            .fontWeight(.semibold)
+        NavigationView{
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVStack{
+                    HStack(spacing: 12){
+                        WebImage(url: user.userImage).placeholder{
+                            // MARK: Placeholder Image
+                            Image("NullProfile")
+                                .resizable()
+                        }
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
                         
-                        Text(user.userEmail ?? "")
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                            .lineLimit(3)
+                        VStack(alignment: .leading, spacing: 6){
+                            Text(user.userName)
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                            Text(user.userEmail ?? "")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                                .lineLimit(3)
+                        }
+                        .hAlign(.leading)
                     }
-                    .hAlign(.leading)
+                    HStack{
+                        VStack{
+                            Text("172")
+                                .font(.title)
+                                .font(.system(size:17))
+                                .fontWeight(.bold)
+                            Text("만든 모임")
+                                .font(.system(size:15))
+                                .fontWeight(.light)
+                                .foregroundColor(.gray)
+                        }
+                        VStack{
+                            Text("15")
+                                .font(.title)
+                                .font(.system(size:17))
+                                .fontWeight(.bold)
+                            Text("친구 목록")
+                                .font(.system(size:15))
+                                .fontWeight(.light)
+                                .foregroundColor(.gray)
+                        }
+                        VStack{
+                            
+                        }
+                    }
                 }
-              /*  Text("지난 모임")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.black)
-                    .hAlign(.leading)
-                    .padding(.vertical,15)
-                    
-                ReusableMeetingsView(passedMeeting: true)  */
+                .padding(15)
             }
-            .padding(15)
         }
-       
     }
 }
 
