b4655e7204b43c75ab0ac40683da306c91c19c1d
SonJanghyuk <chsonts@gmail.com>
2023-05-26 03:13:52 +0900

    fdf

--- GatherUp/View/DM/DMView.swift
+++ GatherUp/View/DM/DMView.swift
@@ -15,103 +15,110 @@ struct DMView: View {
     @StateObject private var viewModel: DMViewModel = .init()
     
     @State private var messageText: String = ""
-    @Binding var receiverUID: String?
-//    let receiverUID: String?
+    @Binding var receiverID: String?
+//    let receiverID: String?
     @Binding var showDMView: Bool
     @State private var count: Int = 0 //테스트용 변수
     
     var body: some View {
-        NavigationStack {
-            ScrollView{
-                LazyVStack(alignment: .leading, spacing: 8) {
-                    ForEach(viewModel.messages.indices, id: \.self) { index in
-                        // index = 0 이 제일 최신 메시지
-                        let message = viewModel.messages[index]
-                        // 마지막 메시지면 nil 아니면 전 메시지 출력
-                        let previousMessage = message==viewModel.messages.last ? nil : viewModel.messages[index + 1]
+        if let receiverID {
+            NavigationStack{
+                VStack {
+                    ScrollView{
+                        LazyVStack(alignment: .leading, spacing: 8) {
+                            ForEach(viewModel.messages.indices, id: \.self) { index in
+                                // index = 0 이 제일 최신 메시지
+                                let message = viewModel.messages[index]
+                                // 마지막 메시지면 nil 아니면 전 메시지 출력
+                                let previousMessage = message==viewModel.messages.last ? nil : viewModel.messages[index + 1]
 
-                        let isCurrentUser = message.senderUID == viewModel.currentUID
+                                let isCurrentUser = message.senderUID == viewModel.currentUID
 
-                        DMMessageRow(message: message, identifying: isCurrentUser, name: viewModel.user?.userName, image: viewModel.user?.userImage).flippedUpsideDown()
-                            .onAppear {
-                                count += 1
-                                print("온어피어호출수:\(count)")
-                                print("messageCount:\(viewModel.messages.count)")
+                                DMMessageRow(message: message, identifying: isCurrentUser, name: viewModel.user?.userName, image: viewModel.user?.userImage).flippedUpsideDown()
+                                    .onAppear {
+                                        count += 1
+                                        print("온어피어호출수:\(count)")
+                                        print("messageCount:\(viewModel.messages.count)")
 
-                                if message.id == viewModel.messages.last?.id && viewModel.paginationDoc != nil  {
-                                    guard let docRef = viewModel.dmPeopleRef else{return}
-                                    viewModel.fetchPrevMessage(dmPeopleRef: docRef)
+                                        if message.id == viewModel.messages.last?.id && viewModel.paginationDoc != nil  {
+                                            guard let docRef = viewModel.dmPeopleRef else{return}
+                                            viewModel.fetchPrevMessage(dmPeopleRef: docRef)
+                                        }
+                                    }
+                                
+                                // 날짜 출력
+                                if isNewDay(previousMessage: previousMessage, currentMessage: message) {
+                                    Text(formatDate(message.timestamp)).flippedUpsideDown()
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                        .padding(.top)
+                                        .frame(maxWidth:.infinity)
                                 }
                             }
-                        
-                        // 날짜 출력
-                        if isNewDay(previousMessage: previousMessage, currentMessage: message) {
-                            Text(formatDate(message.timestamp)).flippedUpsideDown()
-                                .font(.caption)
-                                .foregroundColor(.gray)
-                                .padding(.top)
-                                .frame(maxWidth:.infinity)
+                        }
+                    }.flippedUpsideDown()
+                    
+                    Spacer()
+                    HStack{
+                        CustomTextFieldRow(placeholder: Text("메시지를 입력하세요"), text: $messageText)
+                        Button{
+                            if viewModel.dmPeopleRef != nil{
+                                viewModel.sendDM(message: messageText)
+                                messageText = ""
+                            }
+                        }label: {
+                            if viewModel.dmPeopleRef != nil{
+                                Image(systemName: "paperplane.fill")
+                                    .foregroundColor(.white)
+                                    .padding(10)
+                                    .background(Color("lightblue"))
+                                    .cornerRadius(50)
+                            } else {
+                                ProgressView()
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.vertical,10)
+                    .background(Color("gray"))
+                    .cornerRadius(50)
+                    .padding()
+                }
+                .navigationBarTitle("채팅방", displayMode: .inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            showDMView = false
+                            print("뒤로")
+                        }) {
+                            HStack {
+                                Image(systemName: "arrow.left")
+                                Text("뒤로")
+                            }
                         }
                     }
                 }
-            }.flippedUpsideDown()
-            Spacer()
-            HStack{
-                CustomTextFieldRow(placeholder: Text("메시지를 입력하세요"), text: $messageText)
-                Button{
-                    if viewModel.dmPeopleRef != nil{
-                        viewModel.sendDM(message: messageText)
-                        messageText = ""
-                    }
-                }label: {
-                    if viewModel.dmPeopleRef != nil{
-                        Image(systemName: "paperplane.fill")
-                            .foregroundColor(.white)
-                            .padding(10)
-                            .background(Color("lightblue"))
-                            .cornerRadius(50)
+                // .onChange(of: viewModel.dmPeopleRef){ id in
+                //     viewModel.dmListener(dmPeopleRef: id)
+                // }
+                .onAppear {
+                    if let receiverUID {
+                        viewModel.setDMRoom(receiverUID: receiverUID)
+                        viewModel.fetchUser(receiverUID)
                     } else {
-                        ProgressView()
-                    }
-                }
-            }
-            .padding(.horizontal)
-            .padding(.vertical,10)
-            .background(Color("gray"))
-            .cornerRadius(50)
-            .padding()
-            .navigationBarTitle("채팅방", displayMode: .inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
                         showDMView = false
-                        print("뒤로")
-                    }) {
-                        HStack {
-                            Image(systemName: "arrow.left")
-                            Text("뒤로")
-                        }
                     }
                 }
-            }
-        }
-        // .onChange(of: viewModel.dmPeopleRef){ id in
-        //     viewModel.dmListener(dmPeopleRef: id)
-        // }
-        .onAppear {
-            if let receiverUID {
-                viewModel.setDMRoom(receiverUID: receiverUID)
-                viewModel.fetchUser(receiverUID)
-            } else {
-                showDMView = false
-            }
-        }
-        .onDisappear {
-            print("디스어피어")
-            viewModel.ifNoChatRemoveDoc()
-//            viewModel.removeListeners()
-        }
+                .onDisappear {
+                    print("디스어피어")
+                    viewModel.ifNoChatRemoveDoc()
+                    // viewModel.removeListeners()
+                }
                 
+            // }
+        }else{
+            ProgressView()
+        }
     }
     func isNewDay(previousMessage: Message?, currentMessage: Message) -> Bool {
         guard let previousMessage = previousMessage else { return true }
